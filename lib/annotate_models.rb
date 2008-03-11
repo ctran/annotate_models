a2c807b9c18bbbb5f2b0f7c22194947e891d45f2
Joshua Vickery <vickeryj@t61.(none)>
2008-03-11 09:33:19 -0400

    don't include schema version in annotations as that results in massive changes to files that don't need to be changed

--- lib/annotate_models.rb
+++ lib/annotate_models.rb
@@ -112,10 +112,6 @@ module AnnotateModels
     # then pas it to the associated block
     def do_annotations
       header = PREFIX.dup
-      version = ActiveRecord::Migrator.current_version rescue 0
-      if version > 0
-        header << "\n# Schema version: #{version}"
-      end
 
       annotated = []
       get_model_files.each do |file|
