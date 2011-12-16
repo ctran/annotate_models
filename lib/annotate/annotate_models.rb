90e4620edcb2c37ec140245ae0d61e31c4c84737
Mike Mayo <mike@twinenginelabs.com>
2011-12-16 15:40:05 -0600

    removing the AR method_missing override

--- lib/annotate/annotate_models.rb
+++ lib/annotate/annotate_models.rb
@@ -350,11 +350,3 @@ module AnnotateModels
 end
 
 # monkey patches
-
-module ::ActiveRecord
-  class Base
-    def self.method_missing(name, *args)
-      # ignore this, so unknown/unloaded macros won't cause parsing to fail
-    end
-  end
-end
