2c9dd7ec4cc747f1f4634997a91595df9ca1fcc6
cuong.tran <cuong.tran@servicenow.com>
2014-06-16 01:21:08 -0700

    Add a dummy observer and remove annotations

---
+++ spec/integration/rails_4.1.1/app/models/task_observer.rb
@@ -0,0 +1,2 @@
+class TaskObserver < ActiveRecord::Observer
+end
--- spec/integration/rails_4.1.1/test/models/task_test.rb
+++ spec/integration/rails_4.1.1/test/models/task_test.rb
@@ -1,14 +1,3 @@
-# == Schema Information
-#
-# Table name: tasks
-#
-#  id         :integer          not null, primary key
-#  content    :string(255)
-#  status     :integer
-#  created_at :datetime
-#  updated_at :datetime
-#
-
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
