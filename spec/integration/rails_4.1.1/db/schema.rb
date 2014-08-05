3dfe88ad54f3a7059f48f3064c1766beddebc4e5
cuong.tran <cuong.tran@servicenow.com>
2014-08-05 01:27:33 -0700

    Add nested models to integration test project (#200)

--- spec/integration/rails_4.1.1/db/schema.rb
+++ spec/integration/rails_4.1.1/db/schema.rb
@@ -11,11 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140526224112) do
+ActiveRecord::Schema.define(version: 20140705000010) do
+
+  create_table "events", force: true do |t|
+    t.string   "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "tasks", force: true do |t|
     t.string   "content"
-    t.integer  "status"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "content"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
