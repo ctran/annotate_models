e6d1b34f9a5f3b3a8944c7e8dd58212000b7c562
anilmaurya <anilmaurya8dec@gmail.com>
2017-09-20 09:51:08 +0530

    Support earlier version of rails

--- lib/annotate/annotate_models.rb
+++ lib/annotate/annotate_models.rb
@@ -249,7 +249,8 @@ module AnnotateModels
       cols = cols.sort_by(&:name) if options[:sort]
       cols = classified_sort(cols) if options[:classified_sort]
       cols.each do |col|
-        col_type = ((col.type && col.type.type) || (col.sql_type && col.sql_type.to_s))
+        col_type = (col.type || col.sql_type)
+        col_type = col_type.respond_to?(:type) ? col_type.type : col_type.to_s
         attrs = []
         attrs << "default(#{schema_default(klass, col)})" unless col.default.nil? || hide_default?(col_type, options)
         attrs << 'unsigned' if col.respond_to?(:unsigned?) && col.unsigned?
