e3e0bbe9fdcc23db49862cb1f9ce239e765e63b1
0x01f7 <0x01f7@users.noreply.github.com>
2017-12-04 08:29:01 +0800

    Try to get proper loaded model when using Rails eager_load_paths (#535)

--- lib/annotate/annotate_models.rb
+++ lib/annotate/annotate_models.rb
@@ -711,11 +711,11 @@ module AnnotateModels
       model_path = file.gsub(/\.rb$/, '')
       model_dir.each { |dir| model_path = model_path.gsub(/^#{dir}/, '').gsub(/^\//, '') }
       begin
-        get_loaded_model(model_path) || raise(BadModelFileError.new)
+        get_loaded_model(model_path, file) || raise(BadModelFileError.new)
       rescue LoadError
         # this is for non-rails projects, which don't get Rails auto-require magic
         file_path = File.expand_path(file)
-        if File.file?(file_path) && silence_warnings { Kernel.require(file_path) }
+        if File.file?(file_path) && Kernel.require(file_path)
           retry
         elsif model_path =~ /\//
           model_path = model_path.split('/')[1..-1].join('/').to_s
@@ -726,8 +726,24 @@ module AnnotateModels
       end
     end
 
+    # Retrieve loaded model class
+    def get_loaded_model(model_path, file)
+      loaded_model_class = get_loaded_model_by_path(model_path)
+      return loaded_model_class if loaded_model_class
+
+      # We cannot get loaded model when `model_path` is loaded by Rails
+      # auto_load/eager_load paths. Try all possible model paths one by one.
+      absolute_file = File.expand_path(file)
+      model_paths =
+        $LOAD_PATH.select { |path| absolute_file.include?(path) }
+                  .map { |path| absolute_file.sub(path, '').sub(/\.rb$/, '').sub(/^\//, '') }
+      model_paths
+        .map { |path| get_loaded_model_by_path(path) }
+        .find { |loaded_model| !loaded_model.nil? }
+    end
+
     # Retrieve loaded model class by path to the file where it's supposed to be defined.
-    def get_loaded_model(model_path)
+    def get_loaded_model_by_path(model_path)
       ActiveSupport::Inflector.constantize(ActiveSupport::Inflector.camelize(model_path))
     rescue StandardError, LoadError
       # Revert to the old way but it is not really robust
@@ -856,15 +872,6 @@ module AnnotateModels
       [rest_cols, timestamps, associations].each { |a| a.sort_by!(&:name) }
 
       ([id] << rest_cols << timestamps << associations).flatten.compact
-    end
-
-    # Ignore warnings for the duration of the block ()
-    def silence_warnings
-      old_verbose = $VERBOSE
-      $VERBOSE = nil
-      yield
-    ensure
-      $VERBOSE = old_verbose
     end
 
     private
