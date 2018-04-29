d2e3a520638c697328b7b1cc8b63789f339bc39a
Shu Fujita <s.fujita@nard.tech>
2018-04-29 18:06:54 +0900

    Make AnnotateRoutes.#app_routes_map private

--- lib/annotate/annotate_routes.rb
+++ lib/annotate/annotate_routes.rb
@@ -112,23 +112,23 @@ module AnnotateRoutes
 
       [magic_comments, new_content]
     end
-  end
 
-  def self.app_routes_map(options)
-    routes_map = `rake routes`.chomp("\n").split(/\n/, -1)
+    def app_routes_map(options)
+      routes_map = `rake routes`.chomp("\n").split(/\n/, -1)
 
-    # In old versions of Rake, the first line of output was the cwd.  Not so
-    # much in newer ones.  We ditch that line if it exists, and if not, we
-    # keep the line around.
-    routes_map.shift if routes_map.first =~ /^\(in \//
+      # In old versions of Rake, the first line of output was the cwd.  Not so
+      # much in newer ones.  We ditch that line if it exists, and if not, we
+      # keep the line around.
+      routes_map.shift if routes_map.first =~ /^\(in \//
 
-    # Skip routes which match given regex
-    # Note: it matches the complete line (route_name, path, controller/action)
-    if options[:ignore_routes]
-      routes_map.reject! { |line| line =~ /#{options[:ignore_routes]}/ }
+      # Skip routes which match given regex
+      # Note: it matches the complete line (route_name, path, controller/action)
+      if options[:ignore_routes]
+        routes_map.reject! { |line| line =~ /#{options[:ignore_routes]}/ }
+      end
+
+      routes_map
     end
-
-    routes_map
   end
 
   def self.routes_file
