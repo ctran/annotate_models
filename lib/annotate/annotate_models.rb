50edd2e2072780d9ebc13eb21f4e712bbe83e688
Shu Fujita <s.fujita@nard.tech>
2018-04-29 17:45:04 +0900

    Make AnnotateModels.#get_foreign_key_info private

--- lib/annotate/annotate_models.rb
+++ lib/annotate/annotate_models.rb
@@ -101,39 +101,6 @@ module AnnotateModels
 
     attr_writer :root_dir
 
-    def get_foreign_key_info(klass, options = {})
-      fk_info = if options[:format_markdown]
-                  "#\n# ### Foreign Keys\n#\n"
-                else
-                  "#\n# Foreign Keys\n#\n"
-                end
-
-      return '' unless klass.connection.respond_to?(:supports_foreign_keys?) &&
-        klass.connection.supports_foreign_keys? && klass.connection.respond_to?(:foreign_keys)
-
-      foreign_keys = klass.connection.foreign_keys(klass.table_name)
-      return '' if foreign_keys.empty?
-
-      format_name = ->(fk) { options[:show_complete_foreign_keys] ? fk.name : fk.name.gsub(/(?<=^fk_rails_)[0-9a-f]{10}$/, '...') }
-
-      max_size = foreign_keys.map(&format_name).map(&:size).max + 1
-      foreign_keys.sort_by {|fk| [format_name.call(fk), fk.column]}.each do |fk|
-        ref_info = "#{fk.column} => #{fk.to_table}.#{fk.primary_key}"
-        constraints_info = ''
-        constraints_info += "ON DELETE => #{fk.on_delete} " if fk.on_delete
-        constraints_info += "ON UPDATE => #{fk.on_update} " if fk.on_update
-        constraints_info.strip!
-
-        fk_info << if options[:format_markdown]
-                     sprintf("# * `%s`%s:\n#     * **`%s`**\n", format_name.call(fk), constraints_info.blank? ? '' : " (_#{constraints_info}_)", ref_info)
-                   else
-                     sprintf("#  %-#{max_size}.#{max_size}s %s %s", format_name.call(fk), "(#{ref_info})", constraints_info).rstrip + "\n"
-                   end
-      end
-
-      fk_info
-    end
-
     # Add a schema block to a file. If the file already contains
     # a schema info block (a comment starting with "== Schema Information"),
     # check if it matches the block that is already there. If so, leave it be.
@@ -762,6 +729,39 @@ module AnnotateModels
       end
 
       index_info
+    end
+
+    def get_foreign_key_info(klass, options = {})
+      fk_info = if options[:format_markdown]
+                  "#\n# ### Foreign Keys\n#\n"
+                else
+                  "#\n# Foreign Keys\n#\n"
+                end
+
+      return '' unless klass.connection.respond_to?(:supports_foreign_keys?) &&
+        klass.connection.supports_foreign_keys? && klass.connection.respond_to?(:foreign_keys)
+
+      foreign_keys = klass.connection.foreign_keys(klass.table_name)
+      return '' if foreign_keys.empty?
+
+      format_name = ->(fk) { options[:show_complete_foreign_keys] ? fk.name : fk.name.gsub(/(?<=^fk_rails_)[0-9a-f]{10}$/, '...') }
+
+      max_size = foreign_keys.map(&format_name).map(&:size).max + 1
+      foreign_keys.sort_by {|fk| [format_name.call(fk), fk.column]}.each do |fk|
+        ref_info = "#{fk.column} => #{fk.to_table}.#{fk.primary_key}"
+        constraints_info = ''
+        constraints_info += "ON DELETE => #{fk.on_delete} " if fk.on_delete
+        constraints_info += "ON UPDATE => #{fk.on_update} " if fk.on_update
+        constraints_info.strip!
+
+        fk_info << if options[:format_markdown]
+                     sprintf("# * `%s`%s:\n#     * **`%s`**\n", format_name.call(fk), constraints_info.blank? ? '' : " (_#{constraints_info}_)", ref_info)
+                   else
+                     sprintf("#  %-#{max_size}.#{max_size}s %s %s", format_name.call(fk), "(#{ref_info})", constraints_info).rstrip + "\n"
+                   end
+      end
+
+      fk_info
     end
 
     def get_schema_footer_text(_klass, options = {})
