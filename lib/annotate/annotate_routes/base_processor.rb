e15df344b53206ffd42674bf5731b86d145150c8
Shu Fujita <s.fujita@nard.tech>
2019-02-03 18:31:16 +0900

    Add AnnotateRoutes::BaseProcessor

---
+++ lib/annotate/annotate_routes/base_processor.rb
@@ -0,0 +1,49 @@
+# This module provides methods for annotating config/routes.rb.
+module AnnotateRoutes
+  # This class is abstract class of classes adding and removing annotation to config/routes.rb.
+  class BaseProcessor
+    def initialize(options, routes_file)
+      @options = options
+      @routes_file = routes_file
+    end
+
+    def routes_file_exist?
+      File.exist?(routes_file)
+    end
+
+    private
+
+    attr_reader :options, :routes_file
+
+    def existing_text
+      @existing_text ||= File.read(routes_file)
+    end
+
+    # @param new_text [String]
+    # @return [Boolean]
+    def rewrite_contents(new_text)
+      content_changed = content_changed?(new_text)
+
+      abort "annotate error. #{routes_file} needs to be updated, but annotate was run with `--frozen`." if content_changed && frozen?
+
+      if content_changed
+        write(new_text)
+        true
+      else
+        false
+      end
+    end
+
+    def write(text)
+      File.open(routes_file, 'wb') { |f| f.puts(text) }
+    end
+
+    def content_changed?(new_text)
+      existing_text != new_text
+    end
+
+    def frozen?
+      options[:frozen]
+    end
+  end
+end
