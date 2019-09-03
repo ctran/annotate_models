204fb045ea5a6340a602422d35c5de5459b039e2
Andrew W. Lee <git@drewlee.com>
2019-09-02 17:15:21 -0700

    Add --models flag

--- lib/annotate.rb
+++ lib/annotate.rb
@@ -37,7 +37,7 @@ module Annotate
   ].freeze
   OTHER_OPTIONS = [
     :additional_file_patterns, :ignore_columns, :skip_on_db_migrate, :wrapper_open, :wrapper_close,
-    :wrapper, :routes, :hide_limit_column_types, :hide_default_column_types,
+    :wrapper, :routes, :models, :hide_limit_column_types, :hide_default_column_types,
     :ignore_routes, :active_admin
   ].freeze
   PATH_OPTIONS = [
