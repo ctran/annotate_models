a8162558cf8d1bec86df6b7ef8a438b9bc41f488
Kyle Zhao <kzhao.sw@gmail.com>
2019-11-09 09:39:01 -0800

    FIX: Ensure only one line is around the annotation (#669)

--- lib/annotate/annotate_models.rb
+++ lib/annotate/annotate_models.rb
@@ -540,9 +540,9 @@ module AnnotateModels
         new_content = if %w(after bottom).include?(options[position].to_s)
                         magic_comments_block + (old_content.rstrip + "\n\n" + wrapped_info_block)
                       elsif magic_comments_block.empty?
-                        magic_comments_block + wrapped_info_block + "\n" + old_content
+                        magic_comments_block + wrapped_info_block + "\n" + old_content.lstrip
                       else
-                        magic_comments_block + "\n" + wrapped_info_block + "\n" + old_content
+                        magic_comments_block + "\n" + wrapped_info_block + "\n" + old_content.lstrip
                       end
       else
         # replace the old annotation with the new one
