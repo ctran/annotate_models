683f4d680f3b44193ac5d24c51d1a641b3336c58
Shu Fujita <s.fujita@nard.tech>
2020-01-09 04:06:00 +0900

    Refactor AnnotateRoutes::AnnotationProcessor#generate_new_content_array

--- lib/annotate/annotate_routes/annotation_processor.rb
+++ lib/annotate/annotate_routes/annotation_processor.rb
@@ -12,9 +12,12 @@ module AnnotateRoutes
       header = HeaderGenerator.generate(options)
       magic_comments_map, content = MagicCommentsExtractor.execute(content)
       if %w[before top].include?(options[:position_in_routes])
-        header = header << '' if content.first != ''
-        magic_comments_map << '' if magic_comments_map.any?
-        new_content = magic_comments_map + header + content
+        new_content_array = []
+        new_content_array += magic_comments_map
+        new_content_array << '' if magic_comments_map.any?
+        new_content_array += header
+        new_content_array << '' if content.first != ''
+        new_content_array += content
       else
         # Ensure we have adequate trailing newlines at the end of the file to
         # ensure a blank line separating the content from the annotation.
@@ -24,13 +27,13 @@ module AnnotateRoutes
         # the spacer we put in the first time around.
         content.shift if header_position == :before && content.first == ''
 
-        new_content = magic_comments_map + content + header
+        new_content_array = magic_comments_map + content + header
       end
 
       # Make sure we end on a trailing newline.
-      new_content << '' unless new_content.last == ''
+      new_content_array << '' unless new_content_array.last == ''
 
-      new_content
+      new_content_array
     end
   end
 end
