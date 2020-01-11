c30222b6eedde4ffff1c28e83166389817414787
Shu Fujita <s.fujita@nard.tech>
2020-01-12 03:58:58 +0900

    Refactor RSpec for AnnotateModels.resolve_filename

--- spec/lib/annotate/annotate_models_spec.rb
+++ spec/lib/annotate/annotate_models_spec.rb
@@ -1861,50 +1861,59 @@ end
     end
   end
 
-  describe '#resolve_filename' do
-    it 'should return the test path for a model' do
-      filename_template = 'test/unit/%MODEL_NAME%_test.rb'
-      model_name        = 'example_model'
-      table_name        = 'example_models'
-
-      filename = AnnotateModels.resolve_filename(filename_template, model_name, table_name)
-      expect(filename). to eq 'test/unit/example_model_test.rb'
+  describe '.resolve_filename' do
+    subject do
+      AnnotateModels.resolve_filename(filename_template, model_name, table_name)
     end
 
-    it 'should return the additional glob' do
-      filename_template = '/foo/bar/%MODEL_NAME%/testing.rb'
-      model_name        = 'example_model'
-      table_name        = 'example_models'
+    context 'When model_name is "example_model" and table_name is "example_models"' do
+      let(:model_name) { 'example_model' }
+      let(:table_name) { 'example_models' }
 
-      filename = AnnotateModels.resolve_filename(filename_template, model_name, table_name)
-      expect(filename). to eq '/foo/bar/example_model/testing.rb'
+      context "when filename_template is 'test/unit/%MODEL_NAME%_test.rb'" do
+        let(:filename_template) { 'test/unit/%MODEL_NAME%_test.rb' }
+
+        it 'returns the test path for a model' do
+          is_expected.to eq 'test/unit/example_model_test.rb'
+        end
+      end
+
+      context "when filename_template is '/foo/bar/%MODEL_NAME%/testing.rb'" do
+        let(:filename_template) { '/foo/bar/%MODEL_NAME%/testing.rb' }
+
+        it 'returns the additional glob' do
+          is_expected.to eq '/foo/bar/example_model/testing.rb'
+        end
+      end
+
+      context "when filename_template is '/foo/bar/%PLURALIZED_MODEL_NAME%/testing.rb'" do
+        let(:filename_template) { '/foo/bar/%PLURALIZED_MODEL_NAME%/testing.rb' }
+
+        it 'returns the additional glob' do
+          is_expected.to eq '/foo/bar/example_models/testing.rb'
+        end
+      end
+
+      context "when filename_template is 'test/fixtures/%TABLE_NAME%.yml'" do
+        let(:filename_template) { 'test/fixtures/%TABLE_NAME%.yml' }
+
+        it 'returns the fixture path for a model' do
+          is_expected.to eq 'test/fixtures/example_models.yml'
+        end
+      end
     end
 
-    it 'should return the additional glob' do
-      filename_template = '/foo/bar/%PLURALIZED_MODEL_NAME%/testing.rb'
-      model_name        = 'example_model'
-      table_name        = 'example_models'
+    context 'When model_name is "parent/child" and table_name is "parent_children"' do
+      let(:model_name) { 'parent/child' }
+      let(:table_name) { 'parent_children' }
 
-      filename = AnnotateModels.resolve_filename(filename_template, model_name, table_name)
-      expect(filename). to eq '/foo/bar/example_models/testing.rb'
-    end
+      context "when filename_template is 'test/fixtures/%PLURALIZED_MODEL_NAME%.yml'" do
+        let(:filename_template) { 'test/fixtures/%PLURALIZED_MODEL_NAME%.yml' }
 
-    it 'should return the fixture path for a model' do
-      filename_template = 'test/fixtures/%TABLE_NAME%.yml'
-      model_name        = 'example_model'
-      table_name        = 'example_models'
-
-      filename = AnnotateModels.resolve_filename(filename_template, model_name, table_name)
-      expect(filename). to eq 'test/fixtures/example_models.yml'
-    end
-
-    it 'should return the fixture path for a nested model' do
-      filename_template = 'test/fixtures/%PLURALIZED_MODEL_NAME%.yml'
-      model_name        = 'parent/child'
-      table_name        = 'parent_children'
-
-      filename = AnnotateModels.resolve_filename(filename_template, model_name, table_name)
-      expect(filename). to eq 'test/fixtures/parent/children.yml'
+        it 'returns the fixture path for a nested model' do
+          is_expected.to eq 'test/fixtures/parent/children.yml'
+        end
+      end
     end
   end
 
