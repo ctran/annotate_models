8a35c0dced838c1367069cef04a77471b08d68cc
Shu Fujita <s.fujita@nard.tech>
2020-01-13 04:52:47 +0900

    Refactor RSpec for AnnotateModels.get_model_class

--- spec/lib/annotate/annotate_models_spec.rb
+++ spec/lib/annotate/annotate_models_spec.rb
@@ -1350,192 +1350,355 @@ EOS
     end
   end
 
-  describe '#get_model_class' do
-    require 'tmpdir'
-
-    # TODO: use 'files' gem instead
-    def create(file, body = 'hi')
-      file_path = File.join(AnnotateModels.model_dir[0], file)
-      FileUtils.mkdir_p(File.dirname(file_path))
-      File.open(file_path, 'wb') do |f|
-        f.puts(body)
-      end
-      file_path
+  describe '.get_model_class' do # rubocop:disable Metrics/BlockLength
+    before :all do
+      require 'tmpdir'
+      AnnotateModels.model_dir = Dir.mktmpdir('annotate_models')
     end
 
-    def check_class_name(file, class_name)
-      klass = AnnotateModels.get_model_class(File.join(AnnotateModels.model_dir[0], file))
-
-      expect(klass).not_to eq(nil)
-      expect(klass.name).to eq(class_name)
+    # TODO: use 'files' gem instead
+    def create(filename, file_content)
+      File.join(AnnotateModels.model_dir[0], filename).tap do |path|
+        FileUtils.mkdir_p(File.dirname(path))
+        File.open(path, 'wb') do |f|
+          f.puts(file_content)
+        end
+      end
     end
 
     before :each do
-      AnnotateModels.model_dir = Dir.mktmpdir 'annotate_models'
+      create(filename, file_content)
     end
 
-    it 'should work' do
-      create 'foo.rb', <<-EOS
-        class Foo < ActiveRecord::Base
-        end
-      EOS
-      check_class_name 'foo.rb', 'Foo'
+    let :klass do
+      AnnotateModels.get_model_class(File.join(AnnotateModels.model_dir[0], filename))
     end
 
-    it 'should find models with non standard capitalization' do
-      create 'foo_with_capitals.rb', <<-EOS
-        class FooWithCAPITALS < ActiveRecord::Base
-        end
-      EOS
-      check_class_name 'foo_with_capitals.rb', 'FooWithCAPITALS'
-    end
+    context 'when class Foo is defined in "foo.rb"' do
+      let :filename do
+        'foo.rb'
+      end
 
-    it 'should find models inside modules' do
-      create 'bar/foo_inside_bar.rb', <<-EOS
-        module Bar
-          class FooInsideBar < ActiveRecord::Base
-          end
-        end
-      EOS
-      check_class_name 'bar/foo_inside_bar.rb', 'Bar::FooInsideBar'
-    end
-
-    it 'should find AR model when duplicated by a nested model' do
-      create 'foo.rb', <<-EOS
-        class Foo < ActiveRecord::Base
-        end
-      EOS
-
-      create 'bar/foo.rb', <<-EOS
-        class Bar::Foo
-        end
-      EOS
-      check_class_name 'bar/foo.rb', 'Bar::Foo'
-      check_class_name 'foo.rb', 'Foo'
-    end
-
-    it 'should find AR model nested inside a class' do
-      create 'voucher.rb', <<-EOS
-        class Voucher < ActiveRecord::Base
-        end
-      EOS
-
-      create 'voucher/foo.rb', <<-EOS
-        class Voucher
-          class Foo
-          end
-        end
-      EOS
-
-      check_class_name 'voucher.rb', 'Voucher'
-      check_class_name 'voucher/foo.rb', 'Voucher::Foo'
-    end
-
-    it 'should not care about unknown macros' do
-      create 'foo_with_macro.rb', <<-EOS
-        class FooWithMacro < ActiveRecord::Base
-          acts_as_awesome :yah
-        end
-      EOS
-      check_class_name 'foo_with_macro.rb', 'FooWithMacro'
-    end
-
-    it 'should not care about known macros' do
-      create('foo_with_known_macro.rb', <<-EOS)
-        class FooWithKnownMacro < ActiveRecord::Base
-          has_many :yah
-        end
-      EOS
-      check_class_name 'foo_with_known_macro.rb', 'FooWithKnownMacro'
-    end
-
-    it 'should work with class names with ALL CAPS segments' do
-      create('foo_with_capitals.rb', <<-EOS)
-        class FooWithCAPITALS < ActiveRecord::Base
-          acts_as_awesome :yah
+      let :file_content do
+        <<~EOS
+          class Foo < ActiveRecord::Base
           end
         EOS
-      check_class_name 'foo_with_capitals.rb', 'FooWithCAPITALS'
+      end
+
+      it 'works' do
+        expect(klass.name).to eq('Foo')
+      end
     end
 
-    it 'should not complain of invalid multibyte char (USASCII)' do
-      create 'foo_with_utf8.rb', <<-EOS
-        #encoding: utf-8
-        class FooWithUtf8 < ActiveRecord::Base
-          UTF8STRINGS = %w[résumé façon âge]
+    context 'when class name is not capitalized normally' do
+      context 'when class FooWithCAPITALS is defined in "foo_with_capitals.rb"' do
+        let :filename do
+          'foo_with_capitals.rb'
         end
-      EOS
-      check_class_name 'foo_with_utf8.rb', 'FooWithUtf8'
+
+        let :file_content do
+          <<~EOS
+            class FooWithCAPITALS < ActiveRecord::Base
+            end
+          EOS
+        end
+
+        it 'works' do
+          expect(klass.name).to eq('FooWithCAPITALS')
+        end
+      end
     end
 
-    it 'should find models inside modules with non standard capitalization' do
-      create 'bar/foo_inside_capitals_bar.rb', <<-EOS
-        module BAR
-          class FooInsideCapitalsBAR < ActiveRecord::Base
+    context 'when class is defined inside module' do
+      context 'when class Bar::FooInsideBar is defined in "bar/foo_inside_bar.rb"' do
+        let :filename do
+          'bar/foo_inside_bar.rb'
+        end
+
+        let :file_content do
+          <<~EOS
+            module Bar
+              class FooInsideBar < ActiveRecord::Base
+              end
+            end
+          EOS
+        end
+
+        it 'works' do
+          expect(klass.name).to eq('Bar::FooInsideBar')
+        end
+      end
+    end
+
+    context 'when class is defined inside module and class name is not capitalized normally' do
+      context 'when class Bar::FooInsideCapitalsBAR is defined in "bar/foo_inside_capitals_bar.rb"' do
+        let :filename do
+          'bar/foo_inside_capitals_bar.rb'
+        end
+
+        let :file_content do
+          <<~EOS
+            module BAR
+              class FooInsideCapitalsBAR < ActiveRecord::Base
+              end
+            end
+          EOS
+        end
+
+        it 'works' do
+          expect(klass.name).to eq('BAR::FooInsideCapitalsBAR')
+        end
+      end
+    end
+
+    context 'when unknown macros exist in class' do
+      context 'when class FooWithMacro is defined in "foo_with_macro.rb"' do
+        let :filename do
+          'foo_with_macro.rb'
+        end
+
+        let :file_content do
+          <<~EOS
+            class FooWithMacro < ActiveRecord::Base
+              acts_as_awesome :yah
+            end
+          EOS
+        end
+
+        it 'works and does not care about known macros' do
+          expect(klass.name).to eq('FooWithMacro')
+        end
+      end
+
+      context 'when class name is with ALL CAPS segments' do
+        context 'when class is "FooWithCAPITALS" is defined in "foo_with_capitals.rb"' do
+          let :filename do
+            'foo_with_capitals.rb'
+          end
+
+          let :file_content do
+            <<~EOS
+              class FooWithCAPITALS < ActiveRecord::Base
+                acts_as_awesome :yah
+              end
+            EOS
+          end
+
+          it 'works' do
+            expect(klass.name).to eq('FooWithCAPITALS')
           end
         end
-      EOS
-      check_class_name 'bar/foo_inside_capitals_bar.rb', 'BAR::FooInsideCapitalsBAR'
+      end
     end
 
-    it 'should find non-namespaced models inside subdirectories' do
-      create 'bar/non_namespaced_foo_inside_bar.rb', <<-EOS
-        class NonNamespacedFooInsideBar < ActiveRecord::Base
+    context 'when known macros exist in class' do
+      context 'when class FooWithKnownMacro is defined in "foo_with_known_macro.rb"' do
+        let :filename do
+          'foo_with_known_macro.rb'
         end
-      EOS
-      check_class_name 'bar/non_namespaced_foo_inside_bar.rb', 'NonNamespacedFooInsideBar'
+
+        let :file_content do
+          <<~EOS
+            class FooWithKnownMacro < ActiveRecord::Base
+              has_many :yah
+            end
+          EOS
+        end
+
+        it 'works and does not care about known macros' do
+          expect(klass.name).to eq('FooWithKnownMacro')
+        end
+      end
     end
 
-    it 'should find non-namespaced models with non standard capitalization inside subdirectories' do
-      create 'bar/non_namespaced_foo_with_capitals_inside_bar.rb', <<-EOS
-        class NonNamespacedFooWithCapitalsInsideBar < ActiveRecord::Base
+    context 'when the file includes invlaid multibyte chars (USASCII)' do
+      context 'when class FooWithUtf8 is defined in "foo_with_utf8.rb"' do
+        let :filename do
+          'foo_with_utf8.rb'
         end
-      EOS
-      check_class_name 'bar/non_namespaced_foo_with_capitals_inside_bar.rb', 'NonNamespacedFooWithCapitalsInsideBar'
+
+        let :file_content do
+          <<~EOS
+            # encoding: utf-8
+            class FooWithUtf8 < ActiveRecord::Base
+              UTF8STRINGS = %w[résumé façon âge]
+            end
+          EOS
+        end
+
+        it 'works without complaining of invalid multibyte chars' do
+          expect(klass.name).to eq('FooWithUtf8')
+        end
+      end
     end
 
-    it 'should allow known macros' do
-      create('foo_with_known_macro.rb', <<-EOS)
-        class FooWithKnownMacro < ActiveRecord::Base
-          has_many :yah
+    context 'when non-namespaced model is inside subdirectory' do
+      context 'when class NonNamespacedFooInsideBar is defined in "bar/non_namespaced_foo_inside_bar.rb"' do
+        let :filename do
+          'bar/non_namespaced_foo_inside_bar.rb'
         end
-      EOS
-      expect(capturing(:stderr) do
-        check_class_name 'foo_with_known_macro.rb', 'FooWithKnownMacro'
-      end).to eq('')
+
+        let :file_content do
+          <<~EOS
+            class NonNamespacedFooInsideBar < ActiveRecord::Base
+            end
+          EOS
+        end
+
+        it 'works' do
+          expect(klass.name).to eq('NonNamespacedFooInsideBar')
+        end
+      end
+
+      context 'when class name is not capitalized normally' do
+        context 'when class NonNamespacedFooWithCapitalsInsideBar is defined in "bar/non_namespaced_foo_with_capitals_inside_bar.rb"' do
+          let :filename do
+            'bar/non_namespaced_foo_with_capitals_inside_bar.rb'
+          end
+
+          let :file_content do
+            <<~EOS
+              class NonNamespacedFooWithCapitalsInsideBar < ActiveRecord::Base
+              end
+            EOS
+          end
+
+          it 'works' do
+            expect(klass.name).to eq('NonNamespacedFooWithCapitalsInsideBar')
+          end
+        end
+      end
     end
 
-    it 'should not require model files twice' do
-      create 'loaded_class.rb', <<-EOS
-        class LoadedClass < ActiveRecord::Base
-          CONSTANT = 1
+    context 'when class file is loaded twice' do
+      context 'when class LoadedClass is defined in "loaded_class.rb"' do
+        let :filename do
+          'loaded_class.rb'
         end
-      EOS
-      path = File.expand_path('loaded_class', AnnotateModels.model_dir[0])
-      Kernel.load "#{path}.rb"
-      expect(Kernel).not_to receive(:require)
 
-      expect(capturing(:stderr) do
-        check_class_name 'loaded_class.rb', 'LoadedClass'
-      end).to be_blank
+        let :file_content do
+          <<~EOS
+            class LoadedClass < ActiveRecord::Base
+              CONSTANT = 1
+            end
+          EOS
+        end
+
+        before :each do
+          path = File.expand_path(filename, AnnotateModels.model_dir[0])
+          Kernel.load(path)
+          expect(Kernel).not_to receive(:require)
+        end
+
+        it 'does not require model file twice' do
+          expect(klass.name).to eq('LoadedClass')
+        end
+      end
+
+      context 'when class is defined in a subdirectory' do
+        dir = Array.new(8) { (0..9).to_a.sample(random: Random.new) }.join
+
+        context "when class SubdirLoadedClass is defined in \"#{dir}/subdir_loaded_class.rb\"" do
+          before :each do
+            $LOAD_PATH.unshift(File.join(AnnotateModels.model_dir[0], dir))
+
+            path = File.expand_path(filename, AnnotateModels.model_dir[0])
+            Kernel.load(path)
+            expect(Kernel).not_to receive(:require)
+          end
+
+          let :filename do
+            "#{dir}/subdir_loaded_class.rb"
+          end
+
+          let :file_content do
+            <<~EOS
+              class SubdirLoadedClass < ActiveRecord::Base
+                CONSTANT = 1
+              end
+            EOS
+          end
+
+          it 'does not require model file twice' do
+            expect(klass.name).to eq('SubdirLoadedClass')
+          end
+        end
+      end
     end
 
-    it 'should not require model files twice which is inside a subdirectory' do
-      dir = Array.new(8) { (0..9).to_a.sample(random: Random.new) }.join
-      $LOAD_PATH.unshift(File.join(AnnotateModels.model_dir[0], dir))
+    context 'when two class exist' do
+      before :each do
+        create(filename_2, file_content_2)
+      end
 
-      create "#{dir}/subdir_loaded_class.rb", <<-EOS
-        class SubdirLoadedClass < ActiveRecord::Base
-          CONSTANT = 1
+      context 'the base names are duplicated' do
+        let :filename do
+          'foo.rb'
         end
-      EOS
-      path = File.expand_path("#{dir}/subdir_loaded_class", AnnotateModels.model_dir[0])
-      Kernel.load "#{path}.rb"
-      expect(Kernel).not_to receive(:require)
 
-      expect(capturing(:stderr) do
-        check_class_name "#{dir}/subdir_loaded_class.rb", 'SubdirLoadedClass'
-      end).to be_blank
+        let :file_content do
+          <<-EOS
+            class Foo < ActiveRecord::Base
+            end
+          EOS
+        end
+
+        let :filename_2 do
+          'bar/foo.rb'
+        end
+
+        let :file_content_2 do
+          <<-EOS
+            class Bar::Foo
+            end
+          EOS
+        end
+
+        let :klass_2 do
+          AnnotateModels.get_model_class(File.join(AnnotateModels.model_dir[0], filename_2))
+        end
+
+        it 'finds valid model' do
+          expect(klass.name).to eq('Foo')
+          expect(klass_2.name).to eq('Bar::Foo')
+        end
+      end
+
+      context 'one of the classes is nested in another class' do
+        let :filename do
+          'voucher.rb'
+        end
+
+        let :file_content do
+          <<-EOS
+            class Voucher < ActiveRecord::Base
+            end
+          EOS
+        end
+
+        let :filename_2 do
+          'voucher/foo.rb'
+        end
+
+        let :file_content_2 do
+          <<~EOS
+            class Voucher
+              class Foo
+              end
+            end
+          EOS
+        end
+
+        let :klass_2 do
+          AnnotateModels.get_model_class(File.join(AnnotateModels.model_dir[0], filename_2))
+        end
+
+        it 'finds valid model' do
+          expect(klass.name).to eq('Voucher')
+          expect(klass_2.name).to eq('Voucher::Foo')
+        end
+      end
     end
   end
 
