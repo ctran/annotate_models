88bebadd935fb4ce0cceb7f0a5eb58e8feb39530
Shu Fujita <s.fujita@nard.tech>
2020-01-13 03:14:37 +0900

    Refactor RSpec test cases of AnnotateModels.parse_options

--- spec/lib/annotate/annotate_models_spec.rb
+++ spec/lib/annotate/annotate_models_spec.rb
@@ -128,7 +128,7 @@ describe AnnotateModels do # rubocop:disable Metrics/BlockLength
     end
   end
 
-  describe '#parse_options' do
+  describe '.parse_options' do
     let(:options) do
       {
         root_dir: '/root',
@@ -136,20 +136,28 @@ describe AnnotateModels do # rubocop:disable Metrics/BlockLength
       }
     end
 
-    it 'sets @root_dir' do
+    before :each do
       AnnotateModels.send(:parse_options, options)
-      expect(AnnotateModels.instance_variable_get(:@root_dir)).to eq('/root')
     end
 
-    it 'sets @model_dir separated with a comma' do
-      AnnotateModels.send(:parse_options, options)
-      expected = [
-        'app/models',
-        'app/one',
-        'app/two',
-        'app/three'
-      ]
-      expect(AnnotateModels.instance_variable_get(:@model_dir)).to eq(expected)
+    describe '@root_dir' do
+      subject do
+        AnnotateModels.instance_variable_get(:@root_dir)
+      end
+
+      it 'sets @root_dir' do
+        is_expected.to eq('/root')
+      end
+    end
+
+    describe '@model_dir' do
+      subject do
+        AnnotateModels.instance_variable_get(:@model_dir)
+      end
+
+      it 'separates option "model_dir" with commas and sets @model_dir as an array of string' do
+        is_expected.to eq(['app/models', 'app/one', 'app/two', 'app/three'])
+      end
     end
   end
 
