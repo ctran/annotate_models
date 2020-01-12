98efea5c078b249b0696f4f1c9d634226ccfadb4
Shu Fujita <s.fujita@nard.tech>
2020-01-13 03:11:49 +0900

    Refactor RSpec test cases of AnnotateModels.set_defaults

--- spec/lib/annotate/annotate_models_spec.rb
+++ spec/lib/annotate/annotate_models_spec.rb
@@ -857,14 +857,25 @@ EOS
 EOS
   end
 
-  describe '#set_defaults' do
-    it 'should default show_complete_foreign_keys to false' do
-      expect(Annotate::Helpers.true?(ENV['show_complete_foreign_keys'])).to be(false)
+  describe '.set_defaults' do
+    subject do
+      Annotate::Helpers.true?(ENV['show_complete_foreign_keys'])
     end
 
-    it 'should be able to set show_complete_foreign_keys to true' do
-      Annotate.set_defaults('show_complete_foreign_keys' => 'true')
-      expect(Annotate::Helpers.true?(ENV['show_complete_foreign_keys'])).to be(true)
+    context 'when default value of "show_complete_foreign_keys" is not set' do
+      it 'returns false' do
+        is_expected.to be(false)
+      end
+    end
+
+    context 'when default value of "show_complete_foreign_keys" is set' do
+      before do
+        Annotate.set_defaults('show_complete_foreign_keys' => 'true')
+      end
+
+      it 'returns true' do
+        is_expected.to be(true)
+      end
     end
 
     after :each do
