36efd5fe351bfb41b6eee417142e74ff54ad70ff
Shu Fujita <s.fujita@nard.tech>
2020-01-13 02:32:09 +0900

    Refactor RSpec test cases of AnnotateModels.quote

--- spec/lib/annotate/annotate_models_spec.rb
+++ spec/lib/annotate/annotate_models_spec.rb
@@ -64,14 +64,69 @@ describe AnnotateModels do # rubocop:disable Metrics/BlockLength
     double('Column', stubs)
   end
 
-  it { expect(AnnotateModels.quote(nil)).to eql('NULL') }
-  it { expect(AnnotateModels.quote(true)).to eql('TRUE') }
-  it { expect(AnnotateModels.quote(false)).to eql('FALSE') }
-  it { expect(AnnotateModels.quote(25)).to eql('25') }
-  it { expect(AnnotateModels.quote(25.6)).to eql('25.6') }
-  it { expect(AnnotateModels.quote(1e-20)).to eql('1.0e-20') }
-  it { expect(AnnotateModels.quote(BigDecimal('1.2'))).to eql('1.2') }
-  it { expect(AnnotateModels.quote([BigDecimal('1.2')])).to eql(['1.2']) }
+  describe '.quote' do
+    subject do
+      AnnotateModels.quote(value)
+    end
+
+    context 'when the argument is nil' do
+      let(:value) { nil }
+      it 'returns string "NULL"' do
+        is_expected.to eq('NULL')
+      end
+    end
+
+    context 'when the argument is true' do
+      let(:value) { true }
+      it 'returns string "TRUE"' do
+        is_expected.to eq('TRUE')
+      end
+    end
+
+    context 'when the argument is false' do
+      let(:value) { false }
+      it 'returns string "FALSE"' do
+        is_expected.to eq('FALSE')
+      end
+    end
+
+    context 'when the argument is an integer' do
+      let(:value) { 25 }
+      it 'returns the integer as a string' do
+        is_expected.to eq('25')
+      end
+    end
+
+    context 'when the argument is a float number' do
+      context 'when the argument is like 25.6' do
+        let(:value) { 25.6 }
+        it 'returns the float number as a string' do
+          is_expected.to eq('25.6')
+        end
+      end
+
+      context 'when the argument is like 1e-20' do
+        let(:value) { 1e-20 }
+        it 'returns the float number as a string' do
+          is_expected.to eq('1.0e-20')
+        end
+      end
+    end
+
+    context 'when the argument is a BigDecimal number' do
+      let(:value) { BigDecimal('1.2') }
+      it 'returns the float number as a string' do
+        is_expected.to eq('1.2')
+      end
+    end
+
+    context 'when the argument is an array' do
+      let(:value) { [BigDecimal('1.2')] }
+      it 'returns an array of which elements are converted to string' do
+        is_expected.to eq(['1.2'])
+      end
+    end
+  end
 
   describe '#parse_options' do
     let(:options) do
