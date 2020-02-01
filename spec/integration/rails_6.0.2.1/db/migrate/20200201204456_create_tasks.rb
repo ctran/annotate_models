5a2e40e01fa7659b98fe114d2611b28ab5699961
Andrew W. Lee <git@drewlee.com>
2020-02-01 10:49:31 -1000

    Add default values to Task migration

--- spec/integration/rails_6.0.2.1/db/migrate/20200201204456_create_tasks.rb
+++ spec/integration/rails_6.0.2.1/db/migrate/20200201204456_create_tasks.rb
@@ -2,8 +2,8 @@ class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
       t.string :content
-      t.integer :count
-      t.boolean :status
+      t.integer :count, default: 0
+      t.boolean :status, default: false
 
       t.timestamps
     end
