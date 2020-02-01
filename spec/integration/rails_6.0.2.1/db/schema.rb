258630ab2f245e1dd58be8c11bfb9557d8c7238a
Andrew W. Lee <git@drewlee.com>
2020-02-01 10:53:29 -1000

    Run `rails db:migrate` and `rubocop --auto-gen-config`
    * Ran the migration for Task model.
    * Ran Rubocop to ignore files in Rails app.

--- spec/integration/rails_6.0.2.1/db/schema.rb
+++ spec/integration/rails_6.0.2.1/db/schema.rb
@@ -10,5 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_02_01_204456) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "content"
+    t.integer "count", default: 0
+    t.boolean "status", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
 end
