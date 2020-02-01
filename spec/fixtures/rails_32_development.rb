379d4f8aa2a7f03c854308124894b0a791124c47
Andrew W. Lee <git@drewlee.com>
2020-02-01 00:42:19 -0800

    Delete integration test fixtures (#746)
    The integration tests were removed since they were outdated and not working. These are dead files so let's remove them.
    
    #725 #744

--- spec/fixtures/rails_32_development.rb
+++
@@ -1,9 +0,0 @@
-TestApp::Application.configure do
-  config.action_dispatch.best_standards_support = :builtin
-  config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.active_record.mass_assignment_sanitizer = :strict
-  config.active_support.deprecation = :log
-  config.cache_classes = false
-  config.consider_all_requests_local = true
-  config.whiny_nils = true
-end
