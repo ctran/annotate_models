7d6e7aa12a5e3452943721a3d7e1e46721bf7812
Andrew W. Lee <git@drewlee.com>
2020-02-03 19:41:19 -1000

    Add integration test for Rails 5.2.4.1

---
+++ spec/integration/rails_5.2.4.1_spec.rb
@@ -0,0 +1,107 @@
+require 'bundler'
+require 'rspec'
+require 'git'
+
+RSpec.describe 'Integration testing on Rails 5.2.4.1' do
+  let(:app_name) { 'rails_5.2.4.1' }
+
+  let(:project_path) { File.expand_path('../..', __dir__) }
+  let!(:app_path) { File.expand_path(app_name, __dir__) }
+
+  let!(:git) { Git.open(project_path) }
+
+  let(:command) { 'bundle exec annotate --models' }
+  let(:migration_command) { 'bin/rails db:migrate' }
+
+  let(:task_model) do
+    patch = <<~PATCH
+      +# == Schema Information
+      +#
+      +# Table name: tasks
+      +#
+      +#  id         :integer          not null, primary key
+      +#  content    :string
+      +#  count      :integer          default("0")
+      +#  status     :boolean          default("0")
+      +#  created_at :datetime         not null
+      +#  updated_at :datetime         not null
+      +#
+    PATCH
+
+    path = 'app/models/task.rb'
+    {
+      path: include(path),
+      patch: include(patch)
+    }
+  end
+  let(:task_test) do
+    patch = <<~PATCH
+      +# == Schema Information
+      +#
+      +# Table name: tasks
+      +#
+      +#  id         :integer          not null, primary key
+      +#  content    :string
+      +#  count      :integer          default("0")
+      +#  status     :boolean          default("0")
+      +#  created_at :datetime         not null
+      +#  updated_at :datetime         not null
+      +#
+    PATCH
+
+    path = 'test/models/task_test.rb'
+    {
+      path: include(path),
+      patch: include(patch)
+    }
+  end
+  let(:task_fixture) do
+    patch = <<~PATCH
+      +# == Schema Information
+      +#
+      +# Table name: tasks
+      +#
+      +#  id         :integer          not null, primary key
+      +#  content    :string
+      +#  count      :integer          default("0")
+      +#  status     :boolean          default("0")
+      +#  created_at :datetime         not null
+      +#  updated_at :datetime         not null
+      +#
+    PATCH
+
+    path = 'test/fixtures/tasks.yml'
+    {
+      path: include(path),
+      patch: include(patch)
+    }
+  end
+
+  before do
+    Bundler.with_clean_env do
+      Dir.chdir app_path do
+        puts `#{migration_command}`
+      end
+    end
+  end
+
+  after do
+    git.reset_hard
+  end
+
+  it 'annotate models' do
+    Bundler.with_clean_env do
+      Dir.chdir app_path do
+        expect(git.diff.any?).to be_falsy
+
+        puts `#{command}`
+
+        expect(git.diff.entries).to contain_exactly(
+          an_object_having_attributes(task_model),
+          an_object_having_attributes(task_test),
+          an_object_having_attributes(task_fixture)
+        )
+      end
+    end
+  end
+end
