23b058e13889f347ffb245f4529e3440cdc81350
Andrew W. Lee <git@drewlee.com>
2020-02-04 09:02:52 -1000

    Add IntegrationHelper to skip tests that would fail

--- spec/integration/integration_helper.rb
+++ spec/integration/integration_helper.rb
@@ -1,6 +1,25 @@
 class IntegrationHelper
-  def self.run_with_ruby_version?(file_name, ruby_version)
-    puts "file_name: #{file_name}"
-    puts "ruby_version: #{ruby_version}"
+  MIN_RUBY_VERSIONS = {
+    'rails_5.2.4.1' => '>= 2.2.2',
+    'rails_6.0.2.1' => '>= 2.5.0'
+  }.freeze
+
+  def self.able_to_run?(file_path, ruby_version)
+    file_name = File.basename(file_path)
+    rails_app = File.basename(file_name, '_spec.rb')
+    ruby_dependency = MIN_RUBY_VERSIONS[rails_app]
+
+    required_version = Gem::Dependency.new('', ruby_dependency)
+    able_to_run = required_version.match?('', ruby_version)
+
+    unless able_to_run
+      output = "\n" \
+            "Skipping running the integration test for #{file_name}.\n" \
+            "The current version of Ruby is #{ruby_version}, " \
+            "but the integration test requires Ruby #{ruby_dependency}."
+      puts output
+    end
+
+    able_to_run
   end
 end
