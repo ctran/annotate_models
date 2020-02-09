be8c3d92f4c7051e475d166337cc61c66ff82247
Shu Fujita <s.fujita@nard.tech>
2020-02-09 15:52:36 +0900

    Refactor AnnotateModels::SchemaInfo.generate

--- lib/annotate/annotate_models/schema_info.rb
+++ lib/annotate/annotate_models/schema_info.rb
@@ -177,12 +177,11 @@ module AnnotateModels
         # If the index includes another column, print it too.
         if options[:simple_indexes] && klass.table_exists? # Check out if this column is indexed
           indices = retrieve_indexes_from_table(klass)
-          if indices = indices.select { |ind| ind.columns.include? column.name }
-            indices.sort_by(&:name).each do |ind|
-              next if ind.columns.is_a?(String)
-              ind = ind.columns.reject! { |i| i == column.name }
-              attrs << (ind.empty? ? 'indexed' : "indexed => [#{ind.join(', ')}]")
-            end
+          indices.select { |ind| ind.columns.include? column.name }&.sort_by(&:name)&.each do |ind|
+            next if ind.columns.is_a?(String)
+
+            ind = ind.columns.reject! { |i| i == column.name }
+            attrs << (ind.empty? ? 'indexed' : "indexed => [#{ind.join(', ')}]")
           end
         end
 
