8a08d0388c092a4eaa7dc38930325e3a77ed50cd
umanoda <hhenpitu@gmail.com>
2020-06-19 03:32:56 +0900

    fixed: changed columns pattern match

--- lib/annotate/annotate_models.rb
+++ lib/annotate/annotate_models.rb
@@ -375,7 +375,7 @@ module AnnotateModels
       old_header = old_content.match(header_pattern).to_s
       new_header = info_block.match(header_pattern).to_s
 
-      column_pattern = /^#[\t ]+[\w\*\.`]+[\t ]+.+$/
+      column_pattern = /^#[\t ]+[\w\*\.`()]+[\t ]+.+$/
       old_columns = old_header && old_header.scan(column_pattern).sort
       new_columns = new_header && new_header.scan(column_pattern).sort
 
