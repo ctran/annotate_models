3849379993169f893b03957b47e6100d2ea8cef5
Andrew W. Lee <git@drewlee.com>
2021-04-26 10:04:01 -0700

    Update Rails 6.0.2.1 => 6.0.3.6

--- spec/integration/integration_helper.rb
+++ spec/integration/integration_helper.rb
@@ -1,7 +1,7 @@
 class IntegrationHelper
   MIN_RUBY_VERSIONS = {
     'rails_5.2.5' => '>= 2.2.2',
-    'rails_6.0.2.1' => '>= 2.5.0'
+    'rails_6.0.3.6' => '>= 2.5.0'
   }.freeze
 
   def self.able_to_run?(file_path, ruby_version)
