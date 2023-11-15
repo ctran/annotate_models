d9cb25cd3d02ff4e9801c4d8dadb2cb0ffbfae33
Adeynack <Adeynack@users.noreply.github.com>
2023-11-15 21:35:44 +0100

    lint

--- spec/lib/tasks/annotate_models_spec.rb
+++ spec/lib/tasks/annotate_models_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../../spec_helper'
 
-describe 'annotate_models rake task and Annotate.set_defaults' do
+describe 'Annotate annotate_models rake task and Annotate.set_defaults' do # rubocop:disable RSpec/DescribeClass
   before do
     Rake.application = Rake::Application.new
     Rake::Task.define_task('environment')
@@ -9,14 +9,15 @@ describe 'annotate_models rake task and Annotate.set_defaults' do
 
   let(:annotate_models_argument) do
     argument = nil
-    expect(AnnotateModels).to receive(:do_annotations) { |arg| argument = arg }
+    expect(AnnotateModels).to have_received(:do_annotations) { |arg| argument = arg }
     Rake::Task['annotate_models'].invoke
     argument
   end
 
   describe 'with_comment_column' do
-    after { ENV.delete('with_comment_column') }
     subject { annotate_models_argument[:with_comment_column] }
+
+    after { ENV.delete('with_comment_column') }
 
     context 'when Annotate.set_defaults is not called (defaults)' do
       it { is_expected.to be_falsey }
@@ -24,6 +25,7 @@ describe 'annotate_models rake task and Annotate.set_defaults' do
 
     context 'when Annotate.set_defaults sets it to "true"' do
       before { Annotate.set_defaults('with_comment_column' => 'true') }
+
       it { is_expected.to be_truthy }
     end
   end
